817ec9153e74c98452736425b1f991b64bd0bc76
Nick.Haer <nhaer123@gmail.com>
2020-01-08 15:31:20 -0600

    added test route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./apiRoutes');
 
+router.use('/api', apiRoutes);
 
-router.use("/api", apiRoutes)
-
+router.route('/', (req, res) => {
+  res.json('Hullo');
+});
 
 // router.use("/api/markers/", function (req, res) {
 //     res.json("boogaloo")
