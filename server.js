e65fb9ac25a2361fe0c2cb20ca94d74a89164cdf
Nick.Haer <nhaer123@gmail.com>
2020-01-12 15:49:52 -0600

    heroku troubleshooting routes

--- server.js
+++ server.js
@@ -6,22 +6,24 @@ const mongoose = require('mongoose');
 
 const routing = require('./routes');
 
-
 const PORT = process.env.PORT || 8000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.use(routing);
+// app.use(routing);
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/roadRangerdb",
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/roadRangerdb",
+// );
 
+app.get('/', (req, res) => {
+  res.json('gotcha');
+});
 
 app.listen(PORT, function() {
   console.log(`Backend server now listening on PORT ${PORT}!`);
